d887832544cea73ed48a3fe412ffbff436fc52a4
dhkehd2 <dhkehd2@cowave.kr>
2023-05-25 16:14:07 +0900

    컨트롤러 파라미터 transform

---
+++ 6.controller_transform_params/src/users/users.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  getAll() {
+    return `allAll`;
+  }
+
+  getOne(userId: number) {
+    return `getOne id: ${userId}`;
+  }
+
+  search(name: string) {
+    return `search name: ${name}`;
+  }
+
+  deleteOne(userId: number) {
+    return `deleteOne id: ${userId}`;
+  }
+
+  create(userData: CreateUserDto) {
+    return `body: ${JSON.stringify(userData)}`;
+  }
+
+  update(userId: number, updateData) {
+    return `id: ${userId} body: ${JSON.stringify(updateData)}`;
+  }
+}
