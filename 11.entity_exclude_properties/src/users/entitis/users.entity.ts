9dd059866ee6ab692571490453f875ff4488fd82
dhkehd2 <dhkehd2@gmail.com>
2023-05-26 00:21:12 +0900

    엔티티 & 특정 필드 제외하여 반환하는 방법 2가지

---
+++ 11.entity_exclude_properties/src/users/entitis/users.entity.ts
@@ -0,0 +1,21 @@
+import { Exclude } from 'class-transformer';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Users {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ length: 255 })
+  name: string;
+
+  @Exclude()
+  @Column({ length: 255 })
+  password: string;
+
+  @Column()
+  age: number;
+
+  @Column('json', { nullable: true })
+  family: string[];
+}
